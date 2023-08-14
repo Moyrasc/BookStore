1c6e7e8059dc722649b058533ab4ceebb58e278b
Moyrasc <noemisolac@gmail.com>
2023-08-14 12:35:14 +0200

    add context and change component

--- src/components/books/BooksGallery.jsx
+++ src/components/books/BooksGallery.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { BookContainer, Books } from './BooksGalleryStyled'
+import { useBooksContext } from '../../dataContext/DataContext'
 
-const BooksGallery = ({ books, pagesFilter, genreFilter, handleBookSelect }) => {
-  const filteredBooks = books.filter(
+const BooksGallery = () => {
+  const { pagesFilter, genreFilter, handleBookSelect, filteredBooks, selectedBooks } = useBooksContext()
+  // Verifica al hacer click si el libro esta en la lista de selectedbook
+  const handleBookClick = (book) => {
+    if (!selectedBooks.some(selectedBook => selectedBook.book.title === book.book.title)) {
+      handleBookSelect(book)
+    }
+  }
+  // filtro paginas y genero
+  const filteredAvailableBooks = filteredBooks.filter(
     book => book.book.pages >= pagesFilter && (genreFilter === '' || book.book.genre === genreFilter)
   )
+  // almacena una lista de libros disponibles que aún no han sido seleccionados por el usuario y aplica filtro para asegurarse
+  const filteredAndAvailableBooks = filteredAvailableBooks.filter(
+    book => !selectedBooks.some(selectedBook => selectedBook.book.title === book.book.title)
+  )
+
   return (
     <BookContainer>
-      {filteredBooks.map((book, index) => {
+      {filteredAndAvailableBooks.map((book, index) => {
         return (
-          <Books className='flip-card' key={index} onClick={() => handleBookSelect(book)}>
+          <Books className='flip-card' key={index} onClick={() => handleBookClick(book)}>
             <h4>{book.book.title}</h4>
             <div className='flip-card-inner'>
               <div className='flip-card-front'>
